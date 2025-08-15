102b52a3dea3cb1b1a8549630947af820b880177
Bhaskar010216 <braj1044@gmail.com>
2025-08-15 18:06:38 +0530

    Create rg.tf

---
+++ rg.tf
@@ -0,0 +1,37 @@
+terraform { 
+  cloud { 
+    
+    organization = "bhaskar94315" 
+
+    workspaces { 
+      name = "localfile" 
+    } 
+  } 
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+client_id="2b819951-5b0b-4439-a9c4-9f6ad8bcce40"
+client_secret="KN38Q~UH4bw4IEL9rnEUWxOjbvfj4tEpPUSpoaK0"
+tenant_id="7e1b4521-93b6-473d-8e77-cf32c35bff05"
+subscription_id="34a1c0d5-ab81-462c-89db-795e1572cd70"
+}
+
+variable "rgname" {
+  default = "teraform-bhaskar15thaug1"
+}
+variable "location" {
+  default = "West US 2"
+}
+resource "azurerm_resource_group" "example" {
+  name     = var.rgname
+  location = var.location
+}
